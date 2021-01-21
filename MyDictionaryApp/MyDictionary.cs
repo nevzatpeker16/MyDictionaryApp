c55c48402e3e0242c321c4ffed12948499ffdeff
nevzatpeker16 <77233554+nevzatpeker16@users.noreply.github.com>
2021-01-21 21:51:56 +0300

    Add project files.

---
+++ MyDictionaryApp/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryApp
+{
+   
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] Keys;
+        TValue[] Values;
+        //Constructor...
+        public MyDictionary()
+    {
+          Keys = new TKey[0];
+           Values = new TValue[0];
+    //Default olarak oluşturması için constructor a yazdım bunu . 
+    }
+
+      public void AddItem(TKey Key,TValue Value)
+        {
+            TKey[] TempKey = Keys;
+            TValue[] TempValue = Values;
+//Ekleme yapmadan önce ilk dizileri kayıt edecek bir yapı oluşturuldu.
+            Keys = new TKey[Keys.Length + 1];
+            Values = new TValue[Values.Length + 1];
+//Yeni eklenecek olan değerler için uzunluğu şuanki dizinin bir fazlası olan 2 adet yeni dizi oluşturuldu.
+         for(int i = 0; i < TempKey.Length;i++)
+            {
+
+                Keys[i] = TempKey[i];
+                //Yeni keys dizisine eski dizinin elemanları ekleniyor...
+                Values[i] = TempValue[i];
+
+
+            }
+            Keys[Keys.Length - 1] = Key;
+            Values[Values.Length - 1] = Value;
+            //Yeni Değerler eklendi.
+            Console.WriteLine("Değerler eklendi yeni eklenen değerler : Key : {0} , Value : {1} ",Key , Value);
+        }
+
+        public int Length
+        {
+            get { return Keys.Length; }
+        
+        }
+        public  TKey[] ListKeys
+        {
+            get { return Keys; }
+        }
+
+        public  TValue[] ListValues
+        {
+            get { return Values; }
+        }
+    }
+}
+//İlk başta salak gibi tempKey yerine Key yaptım , Key.length tabi 1 olduğu için hata olarak index out of bounds exception aldım ...
+//Array programının aynısı gibi , burada mantığını anladıktan sonra çok kolay ilerleniyor. 
---
+++ MyDictionaryApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyDictionaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            MyDictionary<int ,string > deneme = new MyDictionary<int,string>();
+            deneme.AddItem(1, "Nevzat");
+            deneme.AddItem(2, "Bilge");
+           
+
+            foreach(var key in deneme.ListKeys)
+            {
+                Console.WriteLine(key);
+            }
+            foreach(var Values in deneme.ListValues)
+            {
+                Console.WriteLine(Values);
+            }
+        }
+    }
+}
